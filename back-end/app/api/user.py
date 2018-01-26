dea38b7f46606e4a74d5418f0a62a7a04d8080d4
nautilis <bevanzpf@gmail.com>
2018-01-26 16:09:33 +0800

    add registor api

--- back-end/app/api/user.py
+++ back-end/app/api/user.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, Blueprint, request
+import time
 from app.model.user import User
 user_api = Blueprint('user_api', __name__)
 
@@ -8,10 +9,24 @@ def hello_world():
     print user
     return jsonify({'say':'hello world'})
 
-# @user_api.route('/', methods=['POST'])
-# def create():
-#     username = request.form['username']
-#     schoolid = request.form['id']
-#     password = request.form['password']
+@user_api.route('/', methods=['POST'])
+def create():
+    schoolid = request.form['schoolid']
+    password = request.form['password']
+    print (schoolid, password)
+    existed_user = User.find_user_by_schoolid(schoolid)
+    if existed_user:
+        return jsonify({"status": "200", "error": "1", "message":"schoolid had existed"})
+    else:
+        user = User()
+        user.schoolid = schoolid
+        user.password = password
+        localtime = time.localtime(time.time())
+        year = time.strftime("%Y", localtime)
+        user.sessionno = year
+        result = User.create_user(user)
+        if result == 1:
+            return jsonify({'status': "200", "error":"0", "message":"success"})
+        else:
+            return jsonify({"status": "200", "error": "1", "message": ""})
 
-#     print (username, schoolid, password)
--- back-end/app/model/user.py
+++ back-end/app/model/user.py
@@ -2,13 +2,13 @@ from app.main  import db, Base
 
 class User(Base):
     uid = db.Column(db.Integer, primary_key=True)
-    schoolid = db.Column(db.Integer)
-    username = db.Column(db.String(255))
+    schoolid = db.Column(db.String(255), index=True)
+    username = db.Column(db.String(255), index=True, default="")
     password = db.Column(db.String(255))
-    sessionno = db.Column(db.String(50))
+    sessionno = db.Column(db.String(50), default='0000')
 
-    def __init__(self, username):
-        self.username=username
+    def __init__(self):
+        pass
 
     def __repr__(self):
         return "<User `{}`>".format(self.username)
@@ -18,9 +18,10 @@ class User(Base):
         return cls.query.filter_by(schoolid=schoolid).first()
 
     @classmethod
-    def new_user(cls, User):
+    def create_user(cls, User):
         existed_user = User.find_user_by_schoolid(User.schoolid)
         if not existed_user:
             db.session.add(User)
             db.session.commit()
+            return 1
         
